4248f53809b9f85a3f50343e005adf61fc12fe3b
finndotbin <vvoid127@gmail.com>
2025-01-10 23:27:07 -0800

    change color of line numbers

--- init.lua
+++ init.lua
@@ -1,19 +1,19 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
-vim.keymap.set("i", "jj", "<Esc>")
-vim.keymap.set("i", "kk", "<Esc>l")
+vim.keymap.set('i', 'jj', '<Esc>')
+vim.keymap.set('i', 'kk', '<Esc>l')
 
-vim.keymap.set("n", "<leader>x", vim.cmd.Ex)
+vim.keymap.set('n', '<leader>x', vim.cmd.Ex)
 
-vim.keymap.set("n", "<leader>/", "<Cmd>noh<CR>")
+vim.keymap.set('n', '<leader>/', '<Cmd>noh<CR>')
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.list = true
-vim.opt.listchars = { tab = "⟶ ", trail = "•", nbsp = '␣' }
+vim.opt.listchars = { tab = '⟶ ', trail = '•', nbsp = '␣' }
 
 vim.opt.cursorline = false
 vim.opt.number = true
@@ -23,19 +23,19 @@ vim.opt.showmode = true
 
 vim.opt.scrolloff = 12
 
-vim.opt.clipboard = "unnamed"
+vim.opt.clipboard = 'unnamed'
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
     lazypath,
-  })
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require('lazy').setup 'plugins'
--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,14 +1,23 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  'catppuccin/nvim',
+  name = 'catppuccin',
   lazy = false,
   priority = 1000,
   config = function()
-    require("catppuccin").setup({
-      flavor = "mocha",
-      transparent_background = true,
-    })
+    local flavor = 'mocha'
 
-    vim.cmd.colorscheme "catppuccin"
-  end
+    require('catppuccin').setup {
+      flavor = flavor,
+      transparent_background = true,
+    }
+
+    local colors = require('catppuccin.palettes').get_palette(flavor)
+
+    vim.cmd.colorscheme 'catppuccin'
+
+    vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = colors.surface2, bold = true })
+    vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = colors.pink })
+    vim.api.nvim_set_hl(0, 'LineNr', { fg = colors.pink })
+    vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = colors.surface2 })
+  end,
 }
