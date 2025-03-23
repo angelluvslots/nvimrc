4f152f8b9ae4b92b58ef4e27e674fa5574f5b7da
angelluvslots <vvoid127@gmail.com>
2025-03-22 20:33:07 -0700

    theme stuff

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,7 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    ---[[
     local flavor = 'mocha'
 
     require('catppuccin').setup {
@@ -11,13 +12,13 @@ return {
       transparent_background = true,
     }
 
-    local colors = require('catppuccin.palettes').get_palette(flavor)
-
     vim.cmd.colorscheme 'catppuccin'
 
+    local colors = require('catppuccin.palettes').get_palette(flavor)
     vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = colors.surface2, bold = true })
     vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = colors.pink })
     vim.api.nvim_set_hl(0, 'LineNr', { fg = colors.pink })
     vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = colors.surface2 })
+    --]]
   end,
 }
---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,14 @@
+return {
+  'ellisonleao/gruvbox.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    --[[
+    require('gruvbox').setup {
+      transparent_mode = true,
+      contrast = 'soft',
+    }
+    vim.cmd.colorscheme 'gruvbox'
+    --]]
+  end,
+}
