81702ff160c9d78159a880ff6b8fa91601dbb946
angelluvslots <vvoid127@gmail.com>
2025-04-05 02:02:18 -0700

    a lot

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -91,7 +91,11 @@ return {
     }
 
     require('mason-lspconfig').setup {
-      ensure_installed = {},
+      ensure_installed = {
+        "lua_ls",
+        "rust_analyzer",
+        "clangd",
+      },
       handlers = {
         -- this first function is the "default handler"
         -- it applies to every language server without a "custom handler"
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,6 @@
 return {
   'williamboman/mason.nvim',
   lazy = false,
-  opts = {},
+  opts = {
+  },
 }
